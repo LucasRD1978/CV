5c315893281810f1bb7fc29ced951744e9aa18b9
LucasRD1978 <lrd1978@outlook.com>
2023-02-23 14:43:38 -0300

    Agregado de sección pdf y su boton

--- js/buttonsFunctions.js
+++ js/buttonsFunctions.js
@@ -5,12 +5,14 @@ document.getElementById("edu").addEventListener('click', function () {
     document.getElementById('personalInformation').style.display='none';
     document.getElementById('skills').style.display='none';
     document.getElementById('workExperience').style.display='none';
+    document.getElementById('pdf').style.display='none';
     //botones
     document.getElementById('home').style.display='inline';
     document.getElementById('personal').style.display='inline';
     document.getElementById('skill').style.display='inline';
     document.getElementById('experience').style.display='inline';
     document.getElementById('edu').style.display='none';
+    document.getElementById('pdfdoc').style.display='inline';
 });
 
 document.getElementById('home').addEventListener('click', function () {
@@ -20,12 +22,14 @@ document.getElementById('home').addEventListener('click', function () {
     document.getElementById('personalInformation').style.display='none';
     document.getElementById('skills').style.display='none';
     document.getElementById('workExperience').style.display='none';
+    document.getElementById('pdf').style.display='none';
     //botones
     document.getElementById('home').style.display='none';
     document.getElementById('personal').style.display='inline';
     document.getElementById('skill').style.display='inline';
     document.getElementById('experience').style.display='inline';
     document.getElementById('edu').style.display='inline';
+    document.getElementById('pdfdoc').style.display='inline';
 });
 
 document.getElementById('personal').addEventListener('click', function () {
@@ -35,12 +39,14 @@ document.getElementById('personal').addEventListener('click', function () {
     document.getElementById('personalInformation').style.display='flex';
     document.getElementById('skills').style.display='none';
     document.getElementById('workExperience').style.display='none';
+    document.getElementById('pdf').style.display='none';
     //botones
     document.getElementById('home').style.display='inline';
     document.getElementById('personal').style.display='none';
     document.getElementById('skill').style.display='inline';
     document.getElementById('experience').style.display='inline';
     document.getElementById('edu').style.display='inline';
+    document.getElementById('pdfdoc').style.display='inline';
 });
 
 document.getElementById('skill').addEventListener('click', function () {
@@ -50,12 +56,14 @@ document.getElementById('skill').addEventListener('click', function () {
     document.getElementById('personalInformation').style.display='none';
     document.getElementById('skills').style.display='flex';
     document.getElementById('workExperience').style.display='none';
+    document.getElementById('pdf').style.display='none';
     //botones
     document.getElementById('home').style.display='inline';
     document.getElementById('personal').style.display='inline';
     document.getElementById('skill').style.display='none';
     document.getElementById('experience').style.display='inline';
     document.getElementById('edu').style.display='inline';
+    document.getElementById('pdfdoc').style.display='inline';
 });
 
 document.getElementById('experience').addEventListener('click', function () {
@@ -65,12 +73,30 @@ document.getElementById('experience').addEventListener('click', function () {
     document.getElementById('personalInformation').style.display='none';
     document.getElementById('skills').style.display='none';
     document.getElementById('workExperience').style.display='flex';
+    document.getElementById('pdf').style.display='none';
     //botones
     document.getElementById('home').style.display='inline';
     document.getElementById('personal').style.display='inline';
     document.getElementById('skill').style.display='inline';
     document.getElementById('experience').style.display='none';
     document.getElementById('edu').style.display='inline';
+    document.getElementById('pdfdoc').style.display='inline';
 });
 
+document.getElementById('pdfdoc').addEventListener('click', function (){
+    //div
+    document.getElementById('name').style.display='none';
+    document.getElementById('education').style.display='none';
+    document.getElementById('personalInformation').style.display='none';
+    document.getElementById('skills').style.display='none';
+    document.getElementById('workExperience').style.display='none';
+    document.getElementById('pdf').style.display='flex';
+    //botones
+    document.getElementById('home').style.display='inline';
+    document.getElementById('personal').style.display='inline';
+    document.getElementById('skill').style.display='inline';
+    document.getElementById('experience').style.display='inline';
+    document.getElementById('edu').style.display='inline';
+    document.getElementById('pdfdoc').style.display='none';
+})
 
